26d9a33be390b092ce8ed6707779c202b4f7823e
Evren Ustun <ustunnevren@gmail.com>
2022-06-08 13:19:44 +0300

    Added tableview and navigation controller.

--- CryptoApp/CryptoApp/ViewController.swift
+++ CryptoApp/CryptoApp/ViewController.swift
@@ -11,13 +11,87 @@ import UIKit
 // UI to show cryptos
 // MVVM
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView : UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CryptoTableViewCell.self,
+                           forCellReuseIdentifier: CryptoTableViewCell.identifier)
+        
+        return tableView
+    }()
+    
+    private var viewModels = [CryptoTableViewCellViewModel]()
+    
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.locale = .current
+        formatter.allowsFloats = true
+        formatter.numberStyle = .currency
+        formatter.formatterBehavior = .default
+        
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Crypto Tracker"
+        
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        APICaller.shared.getAllCryptoData { [weak self] result in
+            switch result{
+            case .success(let models):
+                self?.viewModels = models.compactMap({
+                    // NumberFormatter
+                    let price = $0.priceUsd ?? 0
+                    let formatter = ViewController.numberFormatter
+                    let priceString = formatter.string(from: NSNumber(value: price))
+                    
+                    return CryptoTableViewCellViewModel(
+                        name: $0.name ?? "N/A",
+                        symbol: $0.assetId,
+                        price: priceString ?? "N/A"
+                    )
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // MARK: - Tableview func
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell else{
+            fatalError()
+        }
+        
+        cell.configure(with: viewModels[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70 
+    }
+    
 }
 
