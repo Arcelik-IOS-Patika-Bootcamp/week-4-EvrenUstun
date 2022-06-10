ba82832994b1e49568888d729cf734b805c53cdc
Evren Ustun <ustunnevren@gmail.com>
2022-06-10 10:45:57 +0300

    Changed api.

--- CryptoApp/CryptoApp/APICaller.swift
+++ CryptoApp/CryptoApp/APICaller.swift
@@ -10,19 +10,21 @@ import Foundation
 final class APICaller{
     static let shared = APICaller()
     
+    // Constants for api
     private struct Constants {
-        static let apiKey = "2365EB53-C1D2-4765-A671-1111698437C0"
-        static let assetsEnpoint = "https://rest-sandbox.coinapi.io/v1/assets/"
+        static let apiKey = "fa0650d9bde2a272ae3bfa1b51e24a1d14319bb3"
+        static let assetsEnpoint = "https://api.nomics.com/v1"
     }
     
     private init () {}
     
     // MARK: - Public
     
+    // Get all data from api
     public func getAllCryptoData(
         completion: @escaping (Result<[Crypto], Error>) -> Void
     ) {
-        guard let url = URL(string: Constants.assetsEnpoint + "?apikey=" + Constants.apiKey) else{
+        guard let url = URL(string: Constants.assetsEnpoint + "/currencies/ticker?key=" + Constants.apiKey) else{
             return
         }
         
@@ -34,7 +36,6 @@ final class APICaller{
             do{
                 // Decode response
                 let cryptos = try JSONDecoder().decode([Crypto].self, from: data)
-                
                 completion(.success(cryptos))
             }catch{
                 completion(.failure(error))
